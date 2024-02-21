58ca5911b6a4e9f10b7403885fb678338a1d22e4
Gustavo <94932251+gugasth@users.noreply.github.com>
2024-02-21 09:07:04 -0300

    Add files via upload

---
+++ aula2.m
@@ -0,0 +1,41 @@
+clc
+clear all
+close all
+
+A1 = 10;
+A2 = 1;
+A3 = 4;
+
+f1 = 100;
+f2 = 200;
+f3 = 300;
+
+fs = 50*f2;
+Ts = 1/fs;
+T = 1/f1;
+t_final = 2
+t = [0:Ts:t_final];
+x1_t = A1*cos(2*pi*f1*t);
+x2_t = A2*cos(2*pi*f2*t);
+x3_t = A3*cos(2*pi*f3*t);
+x4_t = 2;
+
+x_t = x1_t + x2_t + x3_t + x4_t;
+
+X_f = fft(x_t)/length(x_t);
+X_f = fftshift(X_f);
+passo_f = 1/t_final;
+f = [-fs/2:passo_f:fs/2];
+
+figure(1)
+plot(t,x_t)
+xlim([0 3*T])
+
+figure(2)
+plot(f,abs(X_f))
+grid on
+
+potencia_media = sum(x_t.^2) / length(x_t)
+Pot_AC = var(x_t)
+Pot_DC = mean(x_t)^2
+Valor_medio = (sum(x_t) / length(x_t))
