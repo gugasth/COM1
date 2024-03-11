4c6be1681e4c627146acaf6448ed4bb04d3ebd4b
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2024-03-11 19:25:03 -0300

    update lab1

--- lab1/cod1.m
+++ lab1/cod1.m
@@ -7,14 +7,14 @@ clear all;
 clc;
 close all;
 
-fs = 100e3;
+fs = 1e5;
 f1 = 1e3;
 f2 = 3e3;
 f3 = 5e3;
 A1 = 6;
 A2 = 2;
 A3 = 4;
-ts = 1e-5
+ts = 1/fs
 t = [0:ts:1];
 x1_t = A1 * sin(2*pi*f1*t);
 x2_t = A2 * sin(2*pi*f2*t);
@@ -43,19 +43,23 @@ y1_t = filter(filtro_PB_t, 1, s_t);
 figure(1)
 subplot(2, 2, 1)
 plot(t, s_t)
+title('s_t')
 xlim([0 5*(1/f1)])
 
 subplot(2, 2, 2)
 plot(t, x1_t)
+title('x1_t')
 xlim([0 5*(1/f1)])
 
 subplot(2, 2, 3)
 plot(t, x2_t)
+title('x2_t')
 xlim([0 5*(1/f1)])
 
 
 subplot(2, 2, 4)
 plot(t, x3_t)
+title('x3_t')
 xlim([0 5*(1/f1)])
 
 # ------------------------------------
@@ -63,15 +67,25 @@ xlim([0 5*(1/f1)])
 figure(2)
 subplot(2, 2, 1)
 plot(f, abs(S_f));
+xlim([-4*f2 4*f2])
+title('S_f')
+
 
 subplot(2, 2, 2)
 plot(f, abs(x1_f))
+xlim([-4*f2 4*f2])
+title('x1_f')
 
 subplot(2, 2, 3)
 plot(f, abs(x2_f))
+xlim([-4*f2 4*f2])
+title('x2_f')
 
 subplot(2, 2, 4)
 plot(f, abs(x3_f))
+xlim([-4*f2 4*f2])
+title('x3_f')
+
 #ylim([0 1.2])
 
 #subplot(3, 1, 3)
@@ -82,3 +96,4 @@ pot_media = (norm(s_t).^2) / length(s_t) # potencia média
 
 figure(3)
 pwelch(s_t)
+
