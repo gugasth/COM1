6d2d314564e23808085464d91258acff2a94bcbe
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2024-07-29 20:54:00 -0300

    :bug: fix: arrumando info e vetor de tempo

--- lab6/qam_demodulation.m
+++ lab6/qam_demodulation.m
@@ -3,148 +3,93 @@ clc; clear all; close all;
 pkg load communications;
 pkg load signal;
 
-% Definir a string de bits
-info = '00011000010111000001100001011100000110000101110000011000010111000001100001011100000110000101110000011000010111000001100001011100';
+k = log2(16);
+num_bits = k * 100000;
 
 % Frequência da portadora
 Fc = 2000;
 Fs = 10000; % Taxa de amostragem
-T = 1/Fs;   % Período de amostragem
+
+% fator upsample
+N = 100;
 
 % Parâmetros para o gráfico
-Rb = 1000; % Taxa de bits (exemplo)
+Rb = 1000; % Taxa de bits
 M = 16;    % Número de níveis da modulação QAM
 T_s = log2(M) / Rb; % Tempo de símbolo calculado
 
-T_amostra = T_s / 1000; % Período de amostragem (exemplo: 100 pontos por período de símbolo)
+T_amostra = num_bits / Fs;
 num_periodos = 4; % Número de períodos para plotar
 
 % Tempo do sinal (para 4 períodos)
-t = 0:T_amostra:num_periodos*T_s-T_amostra;
+Fa = N * Rb/log2(M);
+Ta = 1/Fa;
+tempo = num_bits / Rb;
+t = [0:Ta:tempo-Ta];
 
-% Número de bits em cada grupo
-grupo_bits = 4;
 
-% Calcular o número de grupos
-num_grupos = length(info) / grupo_bits;
-
-% Inicializar vetor para armazenar os decimais de 4 em 4 bits
-decimais = zeros(1, num_grupos);
-
-% Converter cada grupo de 4 bits para decimal
-for i = 1:num_grupos
-    % Extrair o grupo de 4 bits
-    grupo_binario = info((i-1)*grupo_bits+1 : i*grupo_bits);
-
-    % Converter o grupo de 4 bits para decimal
-    decimais(i) = bin2dec(grupo_binario);
-end
+info = randint(1, num_bits, 2);
+info = reshape(info, length(info)/k, k);
+info = transpose(bi2de(info));
 
 % Modulação QAM (16-QAM)
-info_complexa = qammod(decimais, M);
+info_complexa = qammod(info, M);
+f1 = scatterplot (info_complexa(1:length(info_complexa)));
+title ('Constelação 16−QAM sem ruído')
+axis([-5 5 -5 5]); % Ajuste o eixo para melhor visualização
 
-% Sinal modulado
-s_t = info_complexa .* exp(1j*2*pi*Fc*t(1:length(info_complexa)));
+info_I = real (info_complexa);
+info_Q = imag(info_complexa);
 
-% Transmissão - Pegando a parte real para transmitir
-s_t_real = real(s_t);
+% Upsample
+info_I_up = upsample(info_I,N);
+info_Q_up = upsample(info_Q,N);
+
+filtro_tx = ones(1,N);
+sinal_I = filter (filtro_tx, 1, info_I_up) ;
+sinal_Q = filter (filtro_tx, 1, info_Q_up);
+
+Portadora_I = (cos(2*pi*Fc.* t (1:length( sinal_I ) ) ) ) ;
+Portadora_Q = (cos(2*pi*Fc.*t(1:length(sinal_Q)) + pi /2) ) ;
+
+sinal_I = sinal_I' .* Portadora_I;
+sinal_Q = sinal_Q' .* Portadora_Q;
+
+sinal_TX = sinal_I - sinal_Q;
 
 % --- Demodulação ---
 
-% Simular o sinal recebido (neste exemplo, sem ruído)
-r_t = s_t_real; % Neste exemplo, o sinal recebido é o sinal transmitido
+%% Recepção do sinal
+filtro_rx = fliplr(filtro_tx);
+SNR = 10;
 
-% Remultiplicar pela portadora complexa conjugada
-r_t_complex = r_t .* exp(-1j*2*pi*Fc*t(1:length(r_t)));
+sinal_RX = awgn(sinal_TX,SNR,'measured');
 
-% Filtrar componentes de alta frequência com um filtro passa-baixas
-filtro_pb = fir1(48, 0.1); % Filtro FIR de ordem 48 com frequência de corte normalizada de 0.1
-r_t_filtrado = filter(filtro_pb, 1, r_t_complex);
+sinal_RX_I = sinal_RX.*Portadora_I;
+sinal_RX_Q = sinal_RX.*-Portadora_Q;
 
-% Extrair partes real e imaginária
-r_real = real(r_t);
-r_imag = imag(r_t);
+sinal_Rx_filtrado_I = filter ( filtro_rx ,1, sinal_RX_I)/N;
+sinal_Rx_filtrado_Q = filter ( filtro_rx ,1, sinal_RX_Q)/N;
 
-% Combinar partes real e imaginária para formar o sinal demodulado
-info_demodulada = r_real + 1j*r_imag;
+f5 = scatterplot ( sinal_Rx_filtrado_I (N:N:end).*2);
+title ('Sinal PAM filtrado (fase)')
 
-% Decodificar a informação demodulada
-decimais_demodulados = qamdemod(info_demodulada, M);
+f6 = scatterplot (sinal_Rx_filtrado_Q(N:N:end).*2);
+title ('Sinal PAM filtrado (quadratura)')
 
-% Converter decimais de volta para binário
-info_reconstituida = '';
-for i = 1:length(decimais_demodulados)
-    info_reconstituida = [info_reconstituida, dec2bin(decimais_demodulados(i), grupo_bits)];
-end
+sinal_16Qam_PLOT = sinal_Rx_filtrado_I(N:N:end).*2 + j*(sinal_Rx_filtrado_Q(N:N:end).*2);
+f7 = scatterplot (sinal_16Qam_PLOT);
+title ('Sinal 16−QAM filtrado')
 
-% Exibir a informação original e a reconstituída
-disp(['Informação original: ', info]);
-disp(['Informação reconstituída: ', info_reconstituida]);
+info_Rx_I = sinal_Rx_filtrado_I (N:N:end).*2;
+info_Rx_Q = sinal_Rx_filtrado_Q(N:N:end).*2;
+info_Rx_Q = info_Rx_Q.*j;
 
+info_Rx = info_Rx_I + info_Rx_Q;
 
-% -------------------------- Plots --------------------------
-% Plot do sinal da informação
-figure(1)
-subplot(1, 2, 1);
-plot(1:length(decimais), decimais)
-title('Sinal de informação no tempo');
-xlabel('Tempo (s)');
-ylabel('Amplitude');
+info_Rx = qamdemod(info_Rx, 16);
 
-subplot(1, 2, 2);
-f = linspace(-Fs/2, Fs/2, length(decimais));
-decimais_f = abs(fftshift(fft(decimais)));
-plot(f, decimais_f);
-title('Espectro do Sinal de informação na freq');
-xlabel('Frequência (Hz)');
-ylabel('Magnitude');
+bits_Rx = reshape(de2bi(info_Rx),[num_bits/k,k]);
 
-% Plot do sinal modulado no tempo
-figure(2);
-subplot(1,2,1);
-plot(t(1:length(s_t_real)), s_t_real);
-title('Sinal Modulado no Tempo (s\_t\_real)');
-xlabel('Tempo (s)');
-ylabel('Amplitude');
+decimais_recebidos = bi2de(bits_Rx);
 
-% Plot do sinal modulado na frequência
-subplot(1,2,2);
-f = linspace(-Fs/2, Fs/2, length(s_t_real));
-S_t_real_f = abs(fftshift(fft(s_t_real)));
-plot(f, S_t_real_f);
-title('Espectro do Sinal Modulado (s\_t\_real)');
-xlabel('Frequência (Hz)');
-ylabel('Magnitude');
-
-% Plot do sinal recebido no tempo
-figure(3)
-subplot(1,2,1);
-plot(t(1:length(r_t)), r_t);
-title('Sinal Recebido no Tempo (r\_t)');
-xlabel('Tempo (s)');
-ylabel('Amplitude');
-
-% Plot do sinal recebido na frequência
-subplot(1,2,2);
-R_t_f = abs(fftshift(fft(r_t)));
-plot(f, R_t_f);
-title('Espectro do Sinal Recebido (r\_t)');
-xlabel('Frequência (Hz)');
-ylabel('Magnitude');
-
-% Plot do sinal demodulado no tempo
-figure(4)
-subplot(1,2,1);
-plot(t(1:length(decimais_demodulados)), decimais_demodulados);
-title('Sinal recebido');
-xlabel('Tempo (s)');
-ylabel('Amplitude');
-
-% Plot do sinal demodulado na frequência
-subplot(1,2,2);
-decimais_demodulados_f = abs(fftshift(fft(decimais_demodulados)));
-plot(f, decimais_demodulados);
-title('Espectro do Sinal recebido');
-xlabel('Frequência (Hz)');
-ylabel('Magnitude');
-legend('r\_real', 'r\_imag');
